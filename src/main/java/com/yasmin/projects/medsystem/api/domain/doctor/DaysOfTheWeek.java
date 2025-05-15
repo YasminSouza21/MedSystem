f2bf44c9e6291ef686c3259b8a00bdfd9f6b0aff
Yasmin <yasmindesouza090@gmail.com>
2025-05-15 10:39:55 -0300

    feat: entity doctor and yours specifications created

---
+++ src/main/java/com/yasmin/projects/medsystem/api/domain/doctor/DaysOfTheWeek.java
@@ -0,0 +1,13 @@
+package com.yasmin.projects.medsystem.api.domain.doctor;
+
+public enum DaysOfTheWeek {
+    SEGUNDA("MONDAY"),
+    TERCA("TUESDAY"),
+    QUARTA("WEDNESDAY"),
+    QUINTA("THURSDAY"),
+    SEXTA("FRIDAY"),
+    SABADO("SATURDAY");
+
+    DaysOfTheWeek(String nameDayOfTheWeekInEnglish){
+    }
+}
